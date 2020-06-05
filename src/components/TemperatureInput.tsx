32570ebfa181f0b6b41b3970db12ac305eb5c357
Ivan Ovdiienko <johnny.unerty@gmail.com>
2020-06-05 18:38:28 +0300

    Add tempetature input

---
+++ src/components/TemperatureInput.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+interface IProps {
+  temperature: number;
+  onInput: (event:any) => void;
+
+}
+
+const TemperatureInput = (props: IProps) =>
+  <div className={"input-card"}>
+    <span>Temperature: <strong> {props.temperature} °C</strong></span>
+    <input style={{width: "280px"}} type={"range"} id="temperatureSelector" name="temperatureSelector" min="-30" max="70" step="1"
+           onInput={(event) => props.onInput(event)}/>
+  </div>;
+
+export default TemperatureInput;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,50 @@
 import * as React from "react";
+import { saturationVaporDensity } from "../functions";
+import TemperatureInput from "../components/TemperatureInput";
 
-// Please note that you can use https://github.com/dotansimha/graphql-code-generator
-// to generate all types from graphQL schema
-interface IndexPageProps {
-  data: {
-    site: {
-      siteMetadata: {
-        title: string;
-      };
+interface IProps {
+}
+
+interface IState {
+  averageTemperature: number; // INPUT Average yearly temperature
+  absoluteHumidity: number; // INPUT Measured in grams per one meter cubic of air
+  treeAmount: number; // INPUT Amount of trees on the territory
+  treeCutting: number; // INPUT How much of trees is cut every year
+  treePlanting: number; // INPUT Trees planted per year
+  precipation: number; // INPUT Measured in mm. Sahara: 25-200, Kalahari: 100-500, Atakama: 25
+  additionalWatering: number; // INPUT Measured in mm. Is added to precipation. May be a river or whatever
+  relativeHumidity: number;
+  volatility: number;
+}
+
+
+export default class extends React.Component<IProps, IState> {
+  constructor(props: IProps, context: any) {
+    super(props, context);
+    this.state = {
+      averageTemperature: 30,
+      absoluteHumidity: 0,
+      treeAmount: 1000,
+      treeCutting: 0,
+      treePlanting: 0,
+      precipation: 50,
+      additionalWatering: 0,
+      relativeHumidity: 0,
+      volatility: 0
     };
-  };
-}
-
-export default function Index(props: IndexPageProps, context: any) {
-  let treeCutting: number = 0; // How much of trees is cut every year
-  let treePlanting: number = 0; // Trees planted per year
-  let precipation: number = 500; // Measured in mm. Sahara: 25-200, Kalahari: 100-500, Atakama: 25
-  let additionalWatering: number = 0; // Measured in mm. Is added to precipation
-  let averageTemperature: number = 30; // Average yearly temperature
-  let absoluteHumidity: number; // Measured in grams per one meter cubic of air
-  let relativeHumidity: number; // https://www.yaklass.ru/p/fizika/8-klass/izmenenie-sostoianiia-veshchestva-141552/otnositelnaia-vlazhnost-vozdukha-i-ee-izmerenie-189576/re-18d24d91-b778-4262-983f-4e1101acae16
-  let volatility: number = 0.0018*(Math.pow(25 + averageTemperature, 2)*(100-relativeHumidity)); // volatility http://meteorologist.ru/formula-isparyaemosti-ivanova.html
-
-
-  return (
-    <div>
-      <h1>Some H1</h1>
-      <p>Some p
-        <strong> Some strong</strong>
-      </p>
-    </div>
-  );
-}
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
   }
-`;
+
+  countRelativeHumidity = (): number => this.state.absoluteHumidity / saturationVaporDensity(this.state.averageTemperature); // https://www.yaklass.ru/p/fizika/8-klass/izmenenie-sostoianiia-veshchestva-141552/otnositelnaia-vlazhnost-vozdukha-i-ee-izmerenie-189576/re-18d24d91-b778-4262-983f-4e1101acae16
+
+  countVolatility = (): number => 0.0018 * (Math.pow(25 + this.state.averageTemperature, 2) * (100 - this.state.relativeHumidity)); // volatility http://meteorologist.ru/formula-isparyaemosti-ivanova.html
+
+
+  public render() {
+    return (
+      <div>
+        <h1>Input Data</h1>
+        <TemperatureInput temperature={this.state.averageTemperature} onInput={(event: any) => this.setState({averageTemperature: event.target.value})}/>
+      </div>
+    );
+  }
+}
