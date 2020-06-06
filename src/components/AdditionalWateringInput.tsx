7cc360464db9f6a3065f50b0c5de6b618a6b5bfd
Ivan Ovdiienko <johnny.unerty@gmail.com>
2020-06-06 15:35:55 +0300

    Add AdditionalWateringInput

---
+++ src/components/AdditionalWateringInput.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+interface IProps {
+  additionalWatering: number;
+  onInput: (event: any) => void;
+
+}
+
+const AdditionalWateringInput = (props: IProps) =>
+  <div className={"input-card"}>
+    <span>AdditionalWatering: <strong> {props.additionalWatering} mm/year</strong></span>
+    <input style={{ borderStyle: "unset", borderRadius: "10px" }}
+           type="number" id="additionalWateringSelector" name="additionalWateringSelector" min="0" max="2000000" step="1000" value={props.additionalWatering}
+           onInput={(event) => props.onInput(event)}
+           onChange={(event) => props.onInput(event)}
+    />
+  </div>;
+
+export default AdditionalWateringInput;
