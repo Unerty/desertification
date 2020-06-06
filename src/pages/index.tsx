7ceb3faac1fba97f31cdbc73301a1b9112c5931c
Ivan Ovdiienko <johnny.unerty@gmail.com>
2020-06-06 19:40:56 +0300

    fix deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,7 @@ export default class extends React.Component<IProps, IState> {
     };
   }
 
-  countAbsoluteHumidity = async (): Promise<number> => await (((this.state.treeAmount * 365.25) + (this.state.cactooAmount * 365.25)) / (this.state.territory)); // ((treeAmount * daysInYear * howMuchEachTreeVaporizesPerDayInGrams) + (same for cactoo)) / (height * SQkmToSQmetersCoefficient * desertTerritory)
+  countAbsoluteHumidity = async (): Promise<number> => await (this.state.treeAmount * 365.25 + this.state.cactooAmount * 365.25) / this.state.territory; // ((treeAmount * daysInYear * howMuchEachTreeVaporizesPerDayInGrams) + (same for cactoo)) / (height * SQkmToSQmetersCoefficient * desertTerritory)
 
   countRelativeHumidity = async (): Promise<number> => await (await this.countAbsoluteHumidity() / saturationVaporDensity(this.state.averageTemperature)); // https://www.yaklass.ru/p/fizika/8-klass/izmenenie-sostoianiia-veshchestva-141552/otnositelnaia-vlazhnost-vozdukha-i-ee-izmerenie-189576/re-18d24d91-b778-4262-983f-4e1101acae16
 
