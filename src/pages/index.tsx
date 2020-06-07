dcf8e4b5bbc5d1a72a06374486f2467a5ff883e7
Ivan Ovdiienko <johnny.unerty@gmail.com>
2020-06-07 11:34:05 +0300

    Add the 40 C rule. If higher - its a desert

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 // https://cyberleninka.ru/article/n/dnevnoy-rashod-vody-na-transpiratsiyu-tselym-drevesnym-rasteniem
 // https://studfile.net/preview/5707905/page:3/ - about volatility
 // http://sun.tsu.ru/mminfo/000063105/274/image/274-136.pdf and http://www.kovas.ru/pdf1/40.pdf about water vaporizing
+// the plant dies above 40C https://iplants.ru/temprezim.htm#:~:text=%D0%A3%20%D0%B1%D0%BE%D0%BB%D1%8C%D1%88%D0%B8%D0%BD%D1%81%D1%82%D0%B2%D0%B0%20%D0%BA%D0%BE%D0%BC%D0%BD%D0%B0%D1%82%D0%BD%D1%8B%D1%85%20%D1%80%D0%B0%D1%81%D1%82%D0%B5%D0%BD%D0%B8%D0%B9%20%D0%B8%D0%BD%D1%82%D0%B5%D0%BD%D1%81%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C,%D0%A1%20%D1%84%D0%BE%D1%82%D0%BE%D1%81%D0%B8%D0%BD%D1%82%D0%B5%D0%B7%20%D0%BF%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%20%D0%BF%D0%BE%D0%BB%D0%BD%D0%BE%D1%81%D1%82%D1%8C%D1%8E%20%D0%BF%D1%80%D0%B5%D0%BA%D1%80%D0%B0%D1%89%D0%B0%D0%B5%D1%82%D1%81%D1%8F.
 
 
 import * as React from "react";
@@ -102,7 +103,8 @@ export default class extends React.Component<IProps, IState> {
 
     if (this.state.averageTemperature < 1) {
       backgroundStyle = "cold-desert";
-    } else {
+    }
+    if (this.state.averageTemperature >= 1 && this.state.averageTemperature < 40) {
       if (humidificationIndex <= DESERT_HUMIDIFICATION_INDEX) {
         backgroundStyle = "desert";
       }
@@ -120,16 +122,19 @@ export default class extends React.Component<IProps, IState> {
         }
       }
       if (humidificationIndex > FOREST_STEPPE_HUMIDIFICATION_INDEX) {
-        backgroundStyle = "forest";
         if (this.state.averageTemperature > 22) {
           backgroundStyle = "rainforest";
         }
         if (this.state.averageTemperature > 9 && this.state.averageTemperature <= 22) {
           backgroundStyle = "forest";
-        } else {
+        }
+        if (this.state.averageTemperature <= 9 ) {
           backgroundStyle = "wetland";
         }
       }
+    }
+    if (this.state.averageTemperature >= 40) {
+      backgroundStyle = "desert";
     }
     return (
       <div>
